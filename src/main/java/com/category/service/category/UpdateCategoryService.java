cc1d88065e0fe2031122c077545665f8d6cc5666
박한빈 <phb0228@naver.com>
2022-06-30 00:57:26 +0900

    카테고리 수정 UseCase를 추가하라

---
+++ src/main/java/com/category/service/category/UpdateCategoryService.java
@@ -0,0 +1,22 @@
+package com.category.service.category;
+
+import com.category.service.category.entity.Category;
+import com.category.service.category.entity.CategoryRepository;
+import com.category.service.category.exception.CategoryNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UpdateCategoryService implements UpdateCategoryUseCase {
+
+  private final CategoryRepository categoryRepository;
+
+  @Override
+  public Category update(long categoryId, UpdateCategoryCommand command) {
+    var findCategory = categoryRepository.findById(categoryId)
+        .orElseThrow(() -> new CategoryNotFoundException(categoryId));
+
+    return findCategory.update(command.getName(), command.getSort());
+  }
+}
