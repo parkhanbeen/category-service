9f61e2ca465e88ab19829fa63be0441040325a0f
박한빈 <phb0228@naver.com>
2022-07-02 18:36:47 +0900

    카테고리 커스텀 레포 테스트를 추가하라

---
+++ src/test/java/com/category/service/category/entity/CategoryCustomRepositoryHelper.java
@@ -0,0 +1,46 @@
+package com.category.service.category.entity;
+
+import java.util.List;
+
+public class CategoryCustomRepositoryHelper {
+
+  public List<Category> createCategories() {
+    var parentCategories = createParentCategories();
+    var childCategories = createChildCategories();
+    childCategories.forEach(parentCategories.get(0)::addChild);
+    return parentCategories;
+  }
+  private List<Category> createParentCategories() {
+    return List.of(
+        Category.builder()
+            .name("카테고리1")
+            .sort(1)
+            .build(),
+        Category.builder()
+            .name("카테고리2")
+            .sort(2)
+            .build(),
+        Category.builder()
+            .name("카테고리3")
+            .sort(3)
+            .build()
+    );
+  }
+
+  private List<Category> createChildCategories() {
+    return List.of(
+        Category.builder()
+            .name("카테고리1-1")
+            .sort(3)
+            .build(),
+        Category.builder()
+            .name("카테고리1-2")
+            .sort(2)
+            .build(),
+        Category.builder()
+            .name("카테고리1-3")
+            .sort(1)
+            .build()
+    );
+  }
+}
