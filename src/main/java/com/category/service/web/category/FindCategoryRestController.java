76d4f7a062de32e251a4a821e5558819da7286db
박한빈 <phb0228@naver.com>
2022-07-02 18:38:31 +0900

    카테고리 전체 조회 엔드포인트를 추가하라

---
+++ src/main/java/com/category/service/web/category/FindCategoryRestController.java
@@ -0,0 +1,23 @@
+package com.category.service.web.category;
+
+import java.util.List;
+
+import com.category.service.category.FindCategoryUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 카테고리 조회 요청을 처리합니다.
+ */
+@RequiredArgsConstructor
+@RestController
+public class FindCategoryRestController {
+
+  private final FindCategoryUseCase findCategoryUseCase;
+
+  @GetMapping("/api/categories")
+  public List<FindCategoryResponse> findAll() {
+    return FindCategoryResponse.of(findCategoryUseCase.findCategories());
+  }
+}
